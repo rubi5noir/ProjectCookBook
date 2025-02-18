ec24fd806ad9edef6a2ce4065b9983a2d01f1710
rubi5noir <pl.dauge@gmail.com>
2025-02-18 11:53:26 +0100

    Ajoutez des fichiers projet.

---
+++ ProjectCookBook/Controllers/RecettesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectCookBook.Controllers
+{
+    public class RecettesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProjectCookBook/Models/Recette.cs
@@ -0,0 +1,16 @@
+﻿namespace ProjectCookBook.Models
+{
+    public class Recette
+    {
+        private int id { get; }
+        private string nom { get; set; }
+        private string description { get; set; }
+        private TimeOnly temps_preparation { get; set; }
+        private TimeOnly temps_cuisson { get; set; }
+        private int difficulte { get; set; }
+
+
+
+
+    }
+}
